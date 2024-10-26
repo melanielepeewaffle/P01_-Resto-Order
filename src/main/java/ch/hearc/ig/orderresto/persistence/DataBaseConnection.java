2e12aa955fe4b5b0bda4014d143fa20df09b7457
Melanie <melanie.lepee@he-arc.ch>
2024-10-26 20:14:54 +0200

    Add transaction management to DataBaseConnection

--- src/main/java/ch/hearc/ig/orderresto/persistence/DataBaseConnection.java
+++ src/main/java/ch/hearc/ig/orderresto/persistence/DataBaseConnection.java
@@ -10,10 +10,52 @@ public class DataBaseConnection {
     private static final String USER = System.getenv("DB_USERNAME");
     private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
+    // Instance de connexion unique
+    private static Connection connection;
+
     // Constructeur privé pour empêcher l'instanciation de cette classe
     private DataBaseConnection() {}
 
+    /**
+     * Obtention de la connexion, avec la gestion d'une seule instance.
+     * Si la connexion est null ou close, elle en crée une nouvelle. Sinon elle réutilise la connexion existante.
+     * @return
+     * @throws SQLException
+     */
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+
+    public static void beginTransaction() throws SQLException {
+        getConnection().setAutoCommit(false);
+    }
+
+    public static void commitTransaction() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.commit();
+            connection.setAutoCommit(true); // Pour les prochaines opérations
+        }
+    }
+
+    public static void rollbackTransaction() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.rollback();
+            connection.setAutoCommit(true); // Pour les prochaines opérations
+        }
+    }
+
+    /**
+     * Ferme explicitement la connexion et la réinitialise à null. Cela permet de garantir que toutes les ressources
+     * sont libérées lorsque la connexion n'est plus nécessaire.
+     * @throws SQLException
+     */
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+            connection = null;
+        }
     }
 }
